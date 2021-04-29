9f479fac77edef9d9a4b7212f65f8c9066a97ba2
FossilFind <33360705+FossilFind@users.noreply.github.com>
2021-04-28 20:05:25 -0500

    Moon Low Gravity

--- src/main/java/fossilfind/scifi/event/EventHandler.java
+++ src/main/java/fossilfind/scifi/event/EventHandler.java
@@ -3,8 +3,9 @@ package fossilfind.scifi.event;
 import fossilfind.scifi.SciFiMod;
 import fossilfind.scifi.init.DimensionInit;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.attributes.RangedAttribute;
-import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -12,12 +13,18 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod.EventBusSubscriber(modid = SciFiMod.MODID, bus = Bus.FORGE)
 public class EventHandler
 {
+	public static final AttributeModifier MOON_GRAVITY = new AttributeModifier("Gravity", (1.62 / 9.8) - 1, AttributeModifier.Operation.MULTIPLY_BASE).setSaved(false);
+	
 	@SubscribeEvent
-	public static void playerTickEvent(PlayerTickEvent event)
+	public static void livingUpdateEvent(LivingUpdateEvent event)
 	{
-		if(event.player.dimension.getModType() == DimensionInit.MOON.get())
-			LivingEntity.ENTITY_GRAVITY = new RangedAttribute(null, "forge.entity_gravity", 0.04D, -8.0D, 8.0D).setShouldWatch(true);
+		IAttributeInstance attribute = event.getEntityLiving().getAttribute(LivingEntity.ENTITY_GRAVITY);
+		
+		if(event.getEntityLiving().dimension.getModType() == DimensionInit.MOON.get())
+			if(!attribute.hasModifier(MOON_GRAVITY))
+				attribute.applyModifier(MOON_GRAVITY);
 		else
-			LivingEntity.ENTITY_GRAVITY = new RangedAttribute(null, "forge.entity_gravity", 0.08D, -8.0D, 8.0D).setShouldWatch(true);
+			if(attribute.hasModifier(MOON_GRAVITY))
+				attribute.removeModifier(MOON_GRAVITY);
 	}
 }
