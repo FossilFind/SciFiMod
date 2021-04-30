e7ee07bdc61ced842e106b1f259a008ad7cf00ad
FossilFind <33360705+FossilFind@users.noreply.github.com>
2021-04-29 21:51:12 -0500

    Fixed recipes and resources

--- src/main/java/fossilfind/scifi/init/BlockInit.java
+++ src/main/java/fossilfind/scifi/init/BlockInit.java
@@ -19,8 +19,8 @@ public class BlockInit
 	
 	public static final RegistryObject<Block> ALUMINUM_BLOCK = BLOCKS.register("aluminum_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
 	public static final RegistryObject<Block> TITANIUM_BLOCK = BLOCKS.register("titanium_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
-	public static final RegistryObject<Block> TANTULUM_BLOCK = BLOCKS.register("tantulum_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
-	public static final RegistryObject<Block> TANTULUM_CARBIDE_BLOCK = BLOCKS.register("tantulum_carbide_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
+	public static final RegistryObject<Block> TANTULUM_BLOCK = BLOCKS.register("tantalum_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
+	public static final RegistryObject<Block> TANTULUM_CARBIDE_BLOCK = BLOCKS.register("tantalum_carbide_block", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK)));
 	public static final RegistryObject<Block> BAUXITE = BLOCKS.register("bauxite", () -> new Block(Block.Properties.from(Blocks.IRON_ORE)));
 	public static final RegistryObject<Block> TANTINITE = BLOCKS.register("tantinite", () -> new Block(Block.Properties.from(Blocks.IRON_ORE)));
 	public static final RegistryObject<Block> RUTILE = BLOCKS.register("rutile", () -> new Block(Block.Properties.from(Blocks.IRON_ORE)));
@@ -28,8 +28,8 @@ public class BlockInit
 	public static final RegistryObject<Block> ALUMINUM_FRAME = BLOCKS.register("aluminum_frame", () -> new Block(Block.Properties.from(Blocks.IRON_BLOCK).notSolid()));
 	public static final RegistryObject<Block> IRON_FRAME = BLOCKS.register("iron_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
 	public static final RegistryObject<Block> TITANIUM_FRAME = BLOCKS.register("titanium_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
-	public static final RegistryObject<Block> TANTULUM_FRAME = BLOCKS.register("tantulum_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
-	public static final RegistryObject<Block> TANTULUM_CARBIDE_FRAME = BLOCKS.register("tantulum_carbide_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
+	public static final RegistryObject<Block> TANTULUM_FRAME = BLOCKS.register("tantalum_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
+	public static final RegistryObject<Block> TANTULUM_CARBIDE_FRAME = BLOCKS.register("tantalum_carbide_frame", () -> new Block(Block.Properties.from(ALUMINUM_FRAME.get())));
 	public static final RegistryObject<Block> LUNAR_REGOLITH = BLOCKS.register("lunar_regolith", () -> new Block(Block.Properties.from(Blocks.SAND)));
 	public static final RegistryObject<Block> REFINERY = BLOCKS.register("refinery", () -> new RefineryBlock(Block.Properties.from(Blocks.FURNACE).lightValue(0).sound(SoundType.ANVIL)));
 	public static final RegistryObject<Block> ELECTROLYZER = BLOCKS.register("electrolyzer", () -> new ElectrolyzerBlock(Block.Properties.from(REFINERY.get())));
